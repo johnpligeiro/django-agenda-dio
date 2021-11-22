a3a247e073625c7100f8c17e1b348709e8f4d32a
johnpligeiro <johnpligeiro@gmail.com>
2021-11-22 10:39:38 -0300

    create list page

--- agenda/urls.py
+++ agenda/urls.py
@@ -15,7 +15,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('agenda/', views.lista_eventos),
+    path('', views.index)
 ]
+
+# from django.contrib import admin
+# from django.urls import path
+# from django.contrib import admin
+# from django.urls import path
+# from core import views
+# from django.views.generic import RedirectView
+#
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('agenda/', views.lista_eventos),
+#     path('', RedirectView.as_view(url='/agenda/'))
+# ]
